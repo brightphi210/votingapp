11120220da1a5941342b0768de746c7ef1326718
Bright Philip <110538612+brightphi210@users.noreply.github.com>
2024-05-09 10:23:34 -0700

    Latest

--- src/Components/LoginCom.jsx
+++ src/Components/LoginCom.jsx
@@ -7,9 +7,9 @@ const LoginCom = () => {
         <div className='w-full text-center justify-center h-full pt-[10rem] '>
             <h2 className='text-7xl font-semibold leading-tight'>Electoral committee of <br /> Namibia(ECN)</h2>
             <p className='py-5'>Vote securely on the blockchain and protect your vote</p>
-            <button class="btn btn-active w-1/2 text-white btn-primary bg-gradient-to-r
+            <button class="btn btn-active w-1/2 text-white text-lg btn-primary bg-gradient-to-r
              from-sky-500 to-indigo-500 border-none hover:bg-purple-100 py-5 px-10 h-[4rem]">
-                Get Started
+                Connect verified wallet
             </button>
         </div>
 
